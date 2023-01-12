6f68dbeebdc523670caf93e77eb4adda81f5aca0
EduardIonescu <eduardionescu23@gmail.com>
2023-01-12 15:35:20 +0100

    changed margin

--- components/countries/countries.jsx
+++ components/countries/countries.jsx
@@ -3,7 +3,7 @@ import CountryCard from "./countryCard";
 export default function Countries({ countriesData, filteredData }) {
 	return (
 		<section
-			className="my-8 md:my-16 xl:my-24 flex flex-wrap flex-col items-center
+			className="my-8 md:my-12 flex flex-wrap flex-col items-center
 			 sm:flex-row 
       xl:gap-x-[calc((100%-(256px*4))/3)] lg:gap-x-[calc((100%-(256px*3))/2)] 
       sm:gap-x-[calc(100%-(256px*2))] gap-y-10 md:gap-y-20"
