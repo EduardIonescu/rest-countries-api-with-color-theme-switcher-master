ed53f3f2325670a4500a9a6b1d95ed3905e854c1
EduardIonescu <eduardionescu23@gmail.com>
2023-01-12 15:31:33 +0100

    search bg bug

--- components/filter/searchBar.jsx
+++ components/filter/searchBar.jsx
@@ -8,8 +8,8 @@ export default function SearchBar({ filterBySearch, setFilterBySearch }) {
 			className={`h-[50px] md:h-16 w-full md:w-[30rem] rounded-md md:rounded-xl
 		border-[2px] text-darkGray dark:text-white focus:outline-none  
 		dark:border-darkBlue transition-all duration-300 
-		bg-[url("../public/icons/search-icon.svg")]
-		dark:bg-[url("../public/icons/dark-search-icon.svg")]
+		bg-[url("/icons/search-icon.svg")]
+		dark:bg-[url("/icons/dark-search-icon.svg")]
 		bg-[center_left_2rem] bg-[length:16px_16px] bg-no-repeat
 		text-[14px] font-semibold placeholder:text-darkGray dark:bg-darkBlue
 		dark:placeholder:text-white pl-20 text-sm md:text-base placeholder:text-xs
